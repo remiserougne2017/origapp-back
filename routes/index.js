4c2af33e4783f4007f8e5a425f14ccc1e531747e
Juliette Subra <juliette.subra@gmail.com>
2020-03-02 18:09:43 +0100

    route catalogue back vraiment ok avec result ok ou ko au front

--- routes/index.js
+++ routes/index.js
@@ -15,19 +15,28 @@ router.get('/', function(req, res, next) {
 router.get('/homePage', async function(req, res, next) {
   console.log("route cata")
  var livreMin = []
-  var catalogue = await booksModel.find()
+ var result = ''
+  var catalogue = await booksModel.find({title: "Bou"})
   console.log(catalogue)
+ if (catalogue.length>0) {
+   result="ok"
   for (let i=0; i<catalogue.length; i++){
      livreMin.push(
     {image: catalogue[i].image,
     title: catalogue[i].title,
     authors: catalogue[i].authors,
+    illustrators: catalogue[i].illustrators,
     rating:catalogue[i].rating
   });
 }
 
   console.log ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^",livreMin)
-  res({livreMin}, "catalogue chargé");
+  res.json({livreMin});
+  console.log("livreMin/catalogue envoyé au front")
+} else {console.log(catalogue);
+result="ko"
+};
+res.json({livreMin, result})
 } )
 
 module.exports = router;
