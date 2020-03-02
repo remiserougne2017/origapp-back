24bf5a749ba571f74cccdfde415c452d5787b583
Juliette Subra <juliette.subra@gmail.com>
2020-03-02 17:40:55 +0100

    route catalogue back ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+
+var booksModel = require('../model/books')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+//// ROUTE CATALOGUE
+
+router.get('/homePage', async function(req, res, next) {
+  console.log("route cata")
+ var livreMin = []
+  var catalogue = await booksModel.find()
+  console.log(catalogue)
+  for (let i=0; i<catalogue.length; i++){
+     livreMin.push(
+    {image: catalogue[i].image,
+    title: catalogue[i].title,
+    authors: catalogue[i].authors,
+    rating:catalogue[i].rating
+  });
+}
+
+  console.log ("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^",livreMin)
+  res({livreMin}, "catalogue chargé");
+} )
 
 module.exports = router;
